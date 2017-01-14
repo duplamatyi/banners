e3c81e421d9cbfff28c6a3ec31c8e219bd52cb15
Matyas Matyas <duplamatyi@gmail.com>
2017-01-14 23:10:39 +0100

    Started using Redis for tracking view_count.

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,12 +2,14 @@ class CampaignsController < ApplicationController
   def show
     redis = Redis.current
     banner_list_key = "banner-list:#{params[:id]}"
-    count = redis.llen banner_list_key
-    if count > 0
-      session_key = "campaign#{params[:id]}"
-      session[session_key] ||= 0
-      @banner_id = redis.lindex(banner_list_key, session[session_key])
-      session[session_key] = (session[session_key] + 1) % count
+    banner_count = redis.llen banner_list_key
+    if banner_count > 0
+      # TODO: Remove this line if integration tests are aware of the session
+      session['dsadsdasadd'] = 'dsdsadsad'
+      view_count_key = "view_count:#{params[:id]}"
+      view_count = session.id.nil? ? 0 : (redis.hget(view_count_key, session.id) || 0)
+      @banner_id = redis.lindex(banner_list_key, view_count)
+      redis.hset(view_count_key, session.id, (view_count.to_i + 1) % banner_count)
     else
       render status: :not_found
     end
